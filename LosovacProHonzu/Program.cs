9f8464111fbb146d14ec824d835c29e76cb5e7ec
AdriansRepos <ferencik.adrian@outlook.cz>
2026-06-08 15:54:47 +0200

    Aplikace pro losování výherců v různých soutěžích

---
+++ LosovacProHonzu/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Zadej soutěžící oddělené mezerou:");
+string? vstup = Console.ReadLine();
+
+string[] soutezici = vstup
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+Console.Write("Kolik výherců chceš vylosovat: ");
+string? pocetInput = Console.ReadLine();
+int pocet;
+
+while (true)
+{
+    if (int.TryParse(pocetInput, out pocet))
+    {
+        if (pocet >= 1 && pocet < soutezici.Length)
+        {
+            break;
+        }
+
+        Console.WriteLine($"Zadej číslo mezi 1 a {soutezici.Length - 1}:");
+    }
+    else
+    {
+        Console.WriteLine("Neplatný počet. Zadej prosím celé číslo:");
+    }
+
+    pocetInput = Console.ReadLine();
+}
+
+Random r = new Random();
+
+var vyherci = soutezici
+    .OrderBy(s => r.Next())
+    .Take(pocet);
+
+Console.WriteLine("Výherci:");
+foreach (string v in vyherci)
+{
+    Console.WriteLine(v);
+}
+
+Console.WriteLine();
+Console.WriteLine("Losování ukončeno. \nStiskni libovolnou klávesu pro uončení programu...");
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,50 @@
+Aplikace pro losování výherců.
+
+Jména zadává uživatel oddělená pouze mezerou.
+
+Počet výherců si může vybrat zadáním čísla.
+
+Nastavena ochrana proti neočekávanému vstupu při zadávání počtu výherců:
+
+číslo musí být rovno 1, nebo vyšší, konečný počet je řešen jako soutěžící - 1.
+
+K ukončení aplikace slouží libovolná klávesa.
+
+Ukázka kódu:
+
+while (true)
+
+{
+
+&#x20;   if (int.TryParse(pocetInput, out pocet))
+
+&#x20;   {
+
+&#x20;       if (pocet >= 1 \&\& pocet < soutezici.Length)
+
+&#x20;       {
+
+&#x20;           break;
+
+&#x20;       }
+
+
+
+&#x20;       Console.WriteLine($"Zadej číslo mezi 1 a {soutezici.Length - 1}:");
+
+&#x20;   }
+
+&#x20;   else
+
+&#x20;   {
+
+&#x20;       Console.WriteLine("Neplatný počet. Zadej prosím celé číslo:");
+
+&#x20;   }
+
+
+
+&#x20;   pocetInput = Console.ReadLine();
+
+}
+
